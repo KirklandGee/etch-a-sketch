46a45e80da56be2cc6b143c117af018ebdbdc0e9
Kirkland Gee <kirklandgee12@gmail.com>
2022-04-27 12:34:27 -0400

    Add grid creation functionality
    The JS is now able to take a number and create a grid of X by X squares.
    It does not take user input yet, but the grid creation is functional.

--- script.js
+++ script.js
@@ -1,14 +1,27 @@
-const size = 16
+const gridArea = document.querySelector('.main')
+const gridSizeSelector = document.querySelector('.slideContainer')
 
-function createGrid (numOfSquares) {
-    for (i=0; i<size; i++) {
-        document.createElement(div)
+
+// gridSizeSelector.addEventListener('mouseup', () => {
+//     let squareNumber = gridSizeSelector.value
+//     console.log(squareNumber)
+// })
+
+function createHorizontalGrid (numOfSquares) {
+    for (i=0; i<numOfSquares; i++) {
+        const square = document.createElement("div")
+        square.className = 'grid-square'
+        gridArea.appendChild(square)
     }
 }
-const square = document.createElement(div)
 
-
-// This number will be a user input of some kind
-
-
-createGrid(size)
+function createFullGrid (numOfSquares) {
+    const mainContainer = document.querySelector(".main")
+    mainContainer.style.gridTemplateRows = `repeat(${numOfSquares}, 1fr)`
+    mainContainer.style.gridTemplateColumns = `repeat(${numOfSquares}, 1fr)`
+    for (i=0; i<(numOfSquares); i++) {
+        createHorizontalGrid(numOfSquares*numOfSquares)
+    }
+    
+}
+createFullGrid(16)

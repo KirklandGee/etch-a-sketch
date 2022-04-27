29b403eda5f4d0b506665bdd5fff9257835a2812
Kirkland Gee <kirklandgee12@gmail.com>
2022-04-27 17:24:03 -0400

    Add grid size selector

--- script.js
+++ script.js
@@ -1,11 +1,6 @@
-const gridArea = document.querySelector('.main')
-const gridSizeSelector = document.querySelector('.slideContainer')
-
-
-// gridSizeSelector.addEventListener('mouseup', () => {
-//     let squareNumber = gridSizeSelector.value
-//     console.log(squareNumber)
-// })
+const gridArea = document.querySelector('.grid')
+const gridSizeSelector = document.querySelector('.slider')
+const mainContainer = document.querySelector(".grid")
 
 function createHorizontalGrid (numOfSquares) {
     for (i=0; i<numOfSquares; i++) {
@@ -16,20 +11,40 @@ function createHorizontalGrid (numOfSquares) {
 }
 
 function createFullGrid (numOfSquares) {
-    const mainContainer = document.querySelector(".main")
     mainContainer.style.gridTemplateRows = `repeat(${numOfSquares}, 1fr)`
     mainContainer.style.gridTemplateColumns = `repeat(${numOfSquares}, 1fr)`
     for (i=0; i<(numOfSquares); i++) {
         createHorizontalGrid(numOfSquares*numOfSquares)
     }
-    
+    colorSquares()
 }
-createFullGrid(16)
 
-const gridSquares = document.querySelectorAll('.grid-squares')
 
-gridSquares.forEach(element => {
-    element.addEventListener('mouseover', () => {
-        gridSquares.style.backgroundColor = 'black'
+function colorSquares () {
+const gridSquares = document.querySelectorAll('.grid-square')
+    gridSquares.forEach(function(element) {
+        element.className = ''
+        element.addEventListener('mouseover', () => {
+            element.className = "black-square"
+            console.log('clicked')
+        })
     })
+}
+
+function resetGrid () {
+    const resetButton = document.querySelector('.reset')
+    resetButton.addEventListener('click', () => {
+        let squareNumber = prompt('How many squares would you like in your grid?')
+        mainContainer.innerHTML = ''
+        //Reset squares in grid
+        createFullGrid(squareNumber)
+    })
+}
+
+const slider = document.querySelector('#myRange')
+slider.addEventListener('mouseup', () => {
+    let squareNumber = slider.value
+    mainContainer.innerHTML = ''
+    createFullGrid(squareNumber)
+    document.querySelector('#grid-size-display').textContent = `${squareNumber} by ${squareNumber}`
 })

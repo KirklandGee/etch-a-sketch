4870e3ff87e131cca999e78531a94a51a85f0906
Kirkland Gee <kirklandgee12@gmail.com>
2022-04-27 11:56:57 -0400

    create bones of HTML doc

---
+++ script.js
@@ -0,0 +1,14 @@
+const size = 16
+
+function createGrid (numOfSquares) {
+    for (i=0; i<size; i++) {
+        document.createElement(div)
+    }
+}
+const square = document.createElement(div)
+
+
+// This number will be a user input of some kind
+
+
+createGrid(size)
